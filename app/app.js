01c6244e401ed1597a020d17e054eb15d93a06e7
Matt <matthewme@pm.me>
2023-12-17 18:07:32 -0600

    wip

--- app/app.js
+++ app/app.js
@@ -1,13 +1,11 @@
-import { REST, Routes } from 'discord.js';
-const http = require('node:http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+var http = require('node:http');
+var hostname = '127.0.0.1';
+var port = 3000;
+var server = http.createServer(function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, function () {
+    console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
 });
